3fb3fee2d81a9365229e12984398652f778c833c
fakehungry <chaikitr@gmail.com>
2026-02-01 12:15:27 +0700

    feat - health-check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+use rust_api::routes::health_check;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
